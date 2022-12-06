59931181721bd7680f30c7ee308c6e082ee29f06
Arindam Bhattacharjee <inferno.arindam@outlook.com>
2022-12-06 17:10:12 +0530

    feat: added circle , quadiriliteral and triangle

---
+++ circle.js
@@ -0,0 +1,22 @@
+const inputRadius = document.getElementById("input-radius");
+const btnCalculate = document.querySelector(".btn-circle-calculate");
+const remarkArea = document.querySelector(".span-remark-area");
+const remarkCircumference = document.querySelector(".span-remark-circumference");
+
+const calculate = (radius) =>{
+    if(radius == 0){
+        remarkArea.textContent = "Please enter value of the radius";
+    }
+    else{
+        let area = radius * radius * Math.PI;
+        let perimeter = 2*Math.PI*radius;
+        remarkArea.textContent = "Area: "+Math.round(area*100)/100+" sq. units";
+        remarkCircumference.textContent = "Circumference: "+ Math.round(perimeter*100)/100  +"  unit";
+    }
+        
+    
+}
+
+btnCalculate.addEventListener('click',()=>{
+    calculate(Number(inputRadius.value));
+});
